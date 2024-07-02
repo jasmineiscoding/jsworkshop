e83bb3c938ab0b4078f34bb5bb7afeea25d43db7
Jasmine Chhetry <jasmine.795424@cct.tu.edu.np>
2024-07-02 17:17:01 +0545

    day 1

---
+++ day1.js
@@ -0,0 +1,29 @@
+// 7 Days JS Workshop
+
+// Day 1 of 7
+
+console.log("Hello World!!") // Writes Hello World!! in console
+
+// Declaring Variables
+
+var friend1 = "Jasmine";
+let friend2 = "Prashant";
+const friend3 = "Titi";
+
+// Arrays are collection of data
+
+const friends = [ "Jasmine" , "Prashant" , "Titi" ];
+
+const name = "Jasmine";
+const age = 19;
+const address = "Dharan";
+
+// Objects are grouped datas with key value pairs
+
+const jasmine = {
+    Name : "Jasmine",
+    Age: 19,
+    Address : "Dharan",
+};
+
+console.log(" Bye World!! ");
